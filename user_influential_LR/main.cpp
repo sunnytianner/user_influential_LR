88da1f42ea47a0edee3168b04d03983bc94b5dbb
Eagle_Han <tian19880220@126.com>
2013-12-08 16:19:48 +0800

    Initial Commit

---
+++ user_influential_LR/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  user_influential_LR
+//
+//  Created by Eagle on 13-12-8.
+//  Copyright (c) 2013年 Eagle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
